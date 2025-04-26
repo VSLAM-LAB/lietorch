2cb482f4b9c491b0103af109338036a27e760e28
alejandrofontan <fontanvillacampa@gmail.com>
2025-04-26 10:11:21 +1000

    Update CUDA architecture flags

--- setup.py
+++ setup.py
@@ -28,7 +28,8 @@ setup(
                     '-gencode=arch=compute_61,code=sm_61', 
                     '-gencode=arch=compute_70,code=sm_70', 
                     '-gencode=arch=compute_75,code=sm_75',
-                    '-gencode=arch=compute_75,code=compute_75',
+                    '-gencode=arch=compute_80,code=sm_80',
+                    '-gencode=arch=compute_86,code=sm_86',  
                     
                 ]
             }),
@@ -49,8 +50,8 @@ setup(
                     '-gencode=arch=compute_61,code=sm_61', 
                     '-gencode=arch=compute_70,code=sm_70', 
                     '-gencode=arch=compute_75,code=sm_75',
-                    '-gencode=arch=compute_75,code=compute_75',
-                    
+                    '-gencode=arch=compute_80,code=sm_80',
+                    '-gencode=arch=compute_86,code=sm_86',      
                 ]
             }),
     ],
